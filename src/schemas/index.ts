ed64548324a4ce3517199d526e0bc8473c8f316c
Michael <mr.m.p.shields@gmail.com>
2024-01-21 18:16:00 +0000

    feat: Update validation messages in LoginSchema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,8 +1,10 @@
 import * as z from 'zod';
 
 export const LoginSchema = z.object({
-    email: z.string().email({
-        message: "A valid email address is required"
-    }),
-    password: z.string()
+  email: z.string().email({
+    message: "Email is required",
+  }),
+  password: z.string().min(1, {
+    message: "Password is required",
+  }),
 });
