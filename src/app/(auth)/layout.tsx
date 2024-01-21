f6e0d1b99ce362ede63f02ceb2f21e43db306ade
Michael <mr.m.p.shields@gmail.com>
2024-01-21 14:50:34 +0000

    style: Add tw styles to AuthLayout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,8 +4,14 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div>
-      <nav className="bg-red-500 text-white">AuthLayout Navbar</nav>
+    <div
+      className="
+      h-full flex 
+      items-center 
+      justify-center       
+      bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] 
+    from-sky-400 to-blue-800"
+    >
       {children}
     </div>
   );
