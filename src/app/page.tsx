45fb9f76f2c975c32e33012f77f0b91187d35c77
Michael <mr.m.p.shields@gmail.com>
2024-01-21 13:58:36 +0000

    feat: root route displays app title and description and signin bttn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,40 @@
-import { Button } from '@/app/components/ui/button';
+import { Poppins } from 'next/font/google';
+
+import { cn } from '@/lib/utils';
+
+import { Button } from './components/ui/button';
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"], 
+});
 
 export default function Home() {
-  return <Button className="font-semibold" size="lg">Click me!</Button>;
+  return (
+    <main
+      className="
+      flex 
+      h-full 
+      flex-col
+      items-center 
+      justify-center 
+      bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] 
+      from-sky-400 to-blue-800"
+    >
+      <div className="text-center space-y-6">
+        <h1 className={cn(
+            font.className,
+            `text-6xl 
+            font-semibold 
+            text-white 
+            drop-shadow-md` 
+          )}
+        >
+          Auth
+        </h1>
+        <p className="text-white text-lg">A simple authentication service</p>
+        <Button variant="secondary" size="lg" className="capitalize">sign in</Button>
+      </div>
+    </main>
+  );
 }
