a4cf67a27f074da4ca18e9ecae86d628893ec7c0
Michael <mr.m.p.shields@gmail.com>
2024-01-21 18:21:14 +0000

    chore: Create LoginForm > onSubmit handler

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -26,6 +26,10 @@ export default function LoginForm() {
     },
   });
 
+  function onSubmit(values: z.infer<typeof LoginSchema>) {
+    console.log({values});
+  }
+
   return (
     <CardWrapper
       backButtonHref="/register"
@@ -34,7 +38,7 @@ export default function LoginForm() {
       showSocial
     >
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(() => {})} className="space-y-6">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
           <div className="space-y-4">
             <FormField
               control={form.control}
