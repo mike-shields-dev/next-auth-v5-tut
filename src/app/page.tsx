33c39a6784dae48a14ec14ca083dbc57318c434c
Michael <mr.m.p.shields@gmail.com>
2024-01-21 13:36:51 +0000

    refactor: Move dir components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@/components/ui/button';
+import { Button } from '@/app/components/ui/button';
 
 export default function Home() {
   return <Button className="font-semibold" size="lg">Click me!</Button>;
