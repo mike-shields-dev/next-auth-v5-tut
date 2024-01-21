b240e0947aa3d1971501d76926c40713bad6f17d
Michael <mr.m.p.shields@gmail.com>
2024-01-21 17:23:28 +0000

    chore: Create Social component

---
+++ src/components/auth/social.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { FaGithub } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
+
+import { Button } from '@/components/ui/button';
+
+export default function Social() {
+  return (
+    <div className="flex items-center w-full gap-x-2">
+      <Button
+        onClick={() => console.log("Sign in with Github")}
+        className="w-full"
+        size="lg"
+        variant="outline"
+      >
+        <FaGithub className="h-5 w-5"/>
+      </Button>
+      <Button
+        onClick={() => console.log("Sign in with Google")}
+        className="w-full"
+        size="lg"
+        variant="outline"
+      >
+        <FcGoogle className="h-5 w-5"/>
+      </Button>
+    </div>
+  );
+}
