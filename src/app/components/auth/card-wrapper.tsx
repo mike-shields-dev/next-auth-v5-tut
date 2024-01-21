849db784fb6b43caae53f7242098d1de80b77067
Michael <mr.m.p.shields@gmail.com>
2024-01-21 14:52:42 +0000

    chore: Create CardWrapper component

---
+++ src/app/components/auth/card-wrapper.tsx
@@ -0,0 +1,23 @@
+import {
+  Card,
+} from '@/components/ui/card';
+
+interface CardWrapperProps {
+  children: React.ReactNode;
+  headerLabel: string;
+  backButtonLabel: string;
+  backButtonHref: string;
+  showSocial?: boolean;
+}
+
+export default function CardWrapper({
+  children,
+  headerLabel,
+  backButtonHref,
+  backButtonLabel,
+  showSocial,
+}: CardWrapperProps) {
+  return <Card className='w-[400px] shadow-md'>
+    {children}
+    </Card>;
+}
