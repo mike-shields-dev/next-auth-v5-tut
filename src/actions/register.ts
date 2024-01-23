33b9e53e47bc321da3c8f0d6a4b2879355bd21fe
Michael <mr.m.p.shields@gmail.com>
2024-01-23 10:17:51 +0000

    register action registers users in db by unique email address

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,7 +1,9 @@
 "use server";
 
+import bcrypt from "bcrypt";
 import { z } from 'zod';
 
+import { db } from "@/lib/db";
 import { RegisterSchema } from '@/schemas';
 
 export async function register(values: z.infer<typeof RegisterSchema>) {
@@ -10,6 +12,27 @@ export async function register(values: z.infer<typeof RegisterSchema>) {
     if(!validatedFields.success) {
         return { error: "Invalid fields"};
     }
+
+    const { name, email, password } = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const isExistingUser = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (isExistingUser) {
+      return { error: "Email already in use" };
+    }
+
+    await db.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword, // IMPORTANT: Always use hashed password
+      },
+    });
 
     return { success: "Email sent" };
 }
