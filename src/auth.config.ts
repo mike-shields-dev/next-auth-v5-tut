9a6dbdcb7ebe25c40aba7cf9d69c9fc6cf9d94ef
Michael <mr.m.p.shields@gmail.com>
2024-01-23 13:52:59 +0000

    chore: Implement Credentials provider for auth middleware

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,6 +1,31 @@
+import bcrypt from "bcryptjs";
 import type { NextAuthConfig } from "next-auth";
-import GitHub from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
+
+import { getUserByEmail } from "./app/data/user";
+import { LoginSchema } from "./schemas";
 
 export default {
-  providers: [GitHub],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await getUserByEmail(email);
+          if (!(user && user.password)) return null;
+
+          const isMatchingPassword = await bcrypt.compare(
+            password,
+            user.password
+          );
+          if (isMatchingPassword) return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
