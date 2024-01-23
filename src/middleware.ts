2c80c6d47810d10e820430817a539ec762ac3e7e
Michael <mr.m.p.shields@gmail.com>
2024-01-23 11:52:28 +0000

    fix: Apply workaround for prisma edge compatability: see guide https://authjs.dev/guides/upgrade-to-v5/#edge-compatibility

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,21 @@
-import { auth } from '@/auth';
+import NextAuth from "next-auth";
+
+import authConfig from "./auth.config";
+
+const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
   const isLoggedIn = !!req.auth;
 
   console.log(req.nextUrl.pathname);
-  console.log({ isLoggedIn })
+  console.log({ isLoggedIn });
 });
 
 export const config = {
-// Configure paths that will call "auth" middleware function:
-// - Those that **DO NOT** end with a file extension or start with "_next".
-// - The root path ("/").
-// - Those that start with "api" or "trpc" (and capture the remaining length).
+  // Configure paths that will call "auth" middleware function:
+  // - Those that **DO NOT** end with a file extension or start with "_next".
+  // - The root path ("/").
+  // - Those that start with "api" or "trpc" (and capture the remaining length).
 
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
