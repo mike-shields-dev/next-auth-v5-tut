4829f59a907636c9e4b7c1fb2a906965bd9f266f
Michael <mr.m.p.shields@gmail.com>
2024-01-23 14:19:00 +0000

    feat: User is redirected to DEFAULT_LOGIN_REDIRECT on successful login

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -1,14 +1,36 @@
 "use server";
+import { AuthError } from 'next-auth';
 import * as z from 'zod';
 
+import { signIn } from '@/auth';
 import { LoginSchema } from '@/schemas';
+
+import { DEFAULT_LOGIN_REDIRECT } from '../../routes';
 
 export async function login(values: z.infer<typeof LoginSchema>) {
   const validatedFields = LoginSchema.safeParse(values);
+  if (!validatedFields.success) return { error: "Invalid fields" };
 
-  if (!validatedFields.success) {
-    return { error: "Invalid fields" };
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials" };
+
+        default:
+          return { error: "Unknown error" };
+      }
+    }
+
+    // Error must be thrown to initiate redirectTo: DEFAULT_LOGIN_REDIRECT
+    throw error; 
   }
-
-  return { success: "Email sent" };
 }
