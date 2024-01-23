e4d3f6333cdf766d1ae5a9e229d1b4f6b1d3752b
Michael <mr.m.p.shields@gmail.com>
2024-01-23 11:43:53 +0000

    chore: Setup auth middleware function and specify triggering routes

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { auth } from '@/auth';
+
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+
+  console.log(req.nextUrl.pathname);
+  console.log({ isLoggedIn })
+});
+
+export const config = {
+// Configure paths that will call "auth" middleware function:
+// - Those that **DO NOT** end with a file extension or start with "_next".
+// - The root path ("/").
+// - Those that start with "api" or "trpc" (and capture the remaining length).
+
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
