352414ffae5b5df9acbb4fd93bf0c08a51b86ac7
Michael <mr.m.p.shields@gmail.com>
2024-01-24 10:28:20 +0000

    refactor: Format file

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,9 +1,9 @@
-import bcrypt from "bcryptjs";
-import type { NextAuthConfig } from "next-auth";
-import Credentials from "next-auth/providers/credentials";
+import bcrypt from 'bcryptjs';
+import type { NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
 
-import { getUserByEmail } from "./app/data/user";
-import { LoginSchema } from "./schemas";
+import { getUserByEmail } from './app/data/user';
+import { LoginSchema } from './schemas';
 
 export default {
   providers: [
